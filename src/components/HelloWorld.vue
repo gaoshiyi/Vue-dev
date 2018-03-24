db80710425f42a6f33f61a33d0b22ccfa6f1c7eb
gaoshiyi <13545106295@163.com>
2018-03-24 23:57:54 +0800

    myResume
    resume

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,82 +1,64 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Learn more about me</h2>
     <ul>
       <li>
         <a
-          href="https://vuejs.org"
+          href="https://github.com/gaoshiyi"
           target="_blank"
         >
-          Core Docs
+          Git
         </a>
       </li>
       <li>
         <a
-          href="https://forum.vuejs.org"
+          href="https://user.qzone.qq.com/2547428066/main"
           target="_blank"
         >
-          Forum
+          QQ2547428066
         </a>
       </li>
       <li>
         <a
-          href="https://chat.vuejs.org"
+          href="https://m.weibo.cn/p/1005053974428214"
           target="_blank"
         >
-          Community Chat
+          Weibo
         </a>
       </li>
       <li>
         <a
-          href="https://twitter.com/vuejs"
+          href="http://www.sunline.cn/aboutus.html"
           target="_blank"
         >
-          Twitter
+        	The last company
+        </a>
+      </li>
+      <li>
+        <a
+          href="file:///C:/Users/syWin10/Downloads/myResume.html"
+          target="_blank"
+        >
+        	My Resume
         </a>
       </li>
       <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
+      
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>Contact me</h2>
     <ul>
       <li>
         <a
-          href="http://router.vuejs.org/"
           target="_blank"
         >
-          vue-router
-        </a>
+          PhoneNum:13545106295        </a>
       </li>
       <li>
-        <a
-          href="http://vuex.vuejs.org/"
+        <a 
           target="_blank"
         >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
+          WechatNum：gaoshiyi63213
         </a>
       </li>
     </ul>
@@ -88,7 +70,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'The resume of Gaoshiyi'
     }
   }
 }
